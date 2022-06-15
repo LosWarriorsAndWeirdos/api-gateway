ca8976b550237e7a23666b3cb448b12afbae4c20
einenekkieinmana <90997198+einenekkieinmana@users.noreply.github.com>
2022-06-14 23:28:07 -0500

    api gateway

---
+++ WebApplication/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT");
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config => config.AddJsonFile($"ocelot.dev.json"));
+                })
+                .ConfigureLogging(logging => logging.AddConsole());
+    }
+}
